d8d46f21b48677c4d51a9ccab0fff43123478824
CobraVmax <andrew.parker@securendpoint.com>
2018-08-09 14:22:26 +0100

    09/08/2018

---
+++ Update Code/driver code.groovy
@@ -0,0 +1,71 @@
+
+
+
+
+// Code snippit version 1.0
+
+
+
+
+
+
+def version(){
+    unschedule()
+    schedule("0 0 8 ? * FRI *", updateCheck)  // Cron schedule - How often to perform the update check - (This example is 8am every Friday)
+    updateCheck()
+}
+
+def updateCheck(){
+    setVersion()
+	def paramsUD = [uri: "http://someDomain.com/versions.json" ]  // This is the URI & path to your hosted JSON file
+       	try {
+        httpGet(paramsUD) { respUD ->
+ //  log.warn " Version Checking - Response Data: ${respUD.data}"   // Troubleshooting Debug Code - Uncommenting this line should show the JSON response from your webserver
+       		def copyrightRead = (respUD.data.copyright)
+       		state.Copyright = copyrightRead
+            def newVerRaw = (respUD.data.versions.Driver.(state.InternalName))
+            def newVer = (respUD.data.versions.Driver.(state.InternalName).replace(".", ""))
+       		def currentVer = state.Version.replace(".", "")
+      		state.UpdateInfo = (respUD.data.versions.UpdateInfo.Driver.(state.InternalName))
+                state.author = (respUD.data.author)
+           
+		if(newVer == "NLS"){
+            state.status = "<b>** This driver is no longer supported by $state.author  **</b>"       
+            log.warn "** This driver is no longer supported by $state.author **"      
+      		}           
+		else if(currentVer < newVer){
+        	state.status = "<b>New Version Available (Version: $newVerRaw)</b>"
+        	log.warn "** There is a newer version of this driver available  (Version: $newVerRaw) **"
+        	log.warn "** $state.UpdateInfo **"
+       		} 
+		else{ 
+      		state.status = "Current"
+      		log.info "You are using the current version of this driver"
+       		}
+      					}
+        	} 
+        catch (e) {
+        	log.error "Something went wrong: CHECK THE JSON FILE AND IT'S URI -  $e"
+    		}
+   		if(state.status == "Current"){
+			state.UpdateInfo = "N/A"
+		    sendEvent(name: "DriverUpdate", value: state.UpdateInfo, isStateChange: true)
+	 	    sendEvent(name: "DriverStatus", value: state.Status, isStateChange: true)
+			}
+    	else{
+	    	sendEvent(name: "DriverUpdate", value: state.UpdateInfo, isStateChange: true)
+	     	sendEvent(name: "DriverStatus", value: state.Status, isStateChange: true)
+	    }   
+ 			sendEvent(name: "DriverAuthor", value: state.author, isStateChange: true)
+    		sendEvent(name: "DriverVersion", value: state.Version, isStateChange: true)
+    
+    
+    	//	
+}
+
+def setVersion(){
+		state.Version = "2.0.0"	 // Version number of this driver
+		state.InternalName = "ExampleDriver1"   // this is the name used in the JSON file for this driver
+}
+
+
