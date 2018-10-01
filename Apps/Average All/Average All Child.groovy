c742f4aae99eb46c07f7b5fa38a1eaaee02c483a
CobraVmax <andrew.parker@securendpoint.com>
2018-10-01 11:39:33 +0100

    Updated 01/10/2018

--- Apps/Average All/Average All Child.groovy
+++ Apps/Average All/Average All Child.groovy
@@ -1,5 +1,5 @@
 /**
- *  ****************  Average All (Illumination, Temperature, Humidity, Pressure)  ****************
+ *  ****************  Average All (Illumination, Temperature, Humidity, Pressure & Motion)  ****************
  *
  *  Design Usage:
  *  This was designed to display/set an 'average' or mean illumination from a group of illumination devices
@@ -35,10 +35,11 @@
  *
  *-------------------------------------------------------------------------------------------------------------------
  *
- *  Last Update: 27/09/2018
+ *  Last Update: 01/10/2018
  *
  *  Changes:
  *
+ *  V1.5.2 - Revised auto update checking and added a manual update check button
  *  V1.5.1 - Debug timers (added 5 second delay before reset to prevent 'bounce')
  *  V1.5.0 - Recoded timers (how often To update virtual device)
  *  V1.4.1 - Debug Motion
@@ -58,7 +59,7 @@ definition(
     name: "Average All Child",
     namespace: "Cobra",
     author: "AJ Parker",
-    description: "This was designed to display/set an 'average' or mean illumination/Humidity/temparature or ambient pressure from a group of devices",
+    description: "This was designed to display/set an 'average' or mean illumination/Humidity/temparature/ambient pressure & motion from a group of devices",
     category: "My Apps",
     
 parent: "Cobra:Average All",
@@ -196,7 +197,7 @@ LOGDEBUG( "Received from: $aveDev - $ave")
     state.mean = 0
     state.mean1 = 0
     state.mean2 = 0
-
+LOGDEBUG("sum = $sum")
     for (sensor in settings.illumSensors) {
     count += 1 
 LOGDEBUG( "Sensor data count = $count" )      
@@ -426,22 +427,61 @@ def LOGDEBUG(txt){
 
 
 def version(){
+    resetBtnName()
 	unschedule()
 	schedule("0 0 9 ? * FRI *", updateCheck) //  Check for updates at 9am every Friday
 	updateCheck()  
+    checkButtons()
 }
 
 def display(){
+  
 	if(state.status){
 	section{paragraph "<img src='http://update.hubitat.uk/icons/cobra3.png''</img> Version: $state.version <br><font face='Lucida Handwriting'>$state.Copyright </font>"}
-	if(state.status != "Current"){
+       
+        }
+    if(state.status != "<b>** This app is no longer supported by $state.author  **</b>"){
+     section(){ input "updateBtn", "button", title: "$state.btnName"}
+    }
+    
+    if(state.status != "Current"){
 	section{ 
-	paragraph "$state.status"
-	paragraph "$state.UpdateInfo"
+
+	paragraph "<b>Update Info: $state.UpdateInfo ***</b>"
     }
+         
+    }         
+}
+
+def checkButtons(){
+    log.info "Running checkButtons"
+    appButtonHandler("updateBtn")
+}
+
+
+def appButtonHandler(btn){
+    state.btnCall = btn
+    if(state.btnCall == "updateBtn"){
+        log.info "Checking for updates now..."
+        updateCheck()
+        pause(3000)
+  		state.btnName = state.newBtn
+        runIn(2, resetBtnName)
     }
-}
-}
+}   
+def resetBtnName(){
+    log.info "Resetting Button"
+    if(state.status != "Current"){
+	state.btnName = state.newBtn
+    }
+    else{
+ state.btnName = "Check For Update" 
+    }
+}    
+    
+
+
+
 
 
 def updateCheck(){
@@ -459,28 +499,38 @@ def updateCheck(){
                 state.author = (respUD.data.author)
            
 		if(newVer == "NLS"){
-            state.status = "<b>** This app is no longer supported by $state.author  **</b>"       
-            log.warn "** This app is no longer supported by $state.author **"      
+            state.status = "<b>** This app is no longer supported by $state.author  **</b>"  
+             log.warn "** This app is no longer supported by $state.author **" 
+            
       		}           
 		else if(currentVer < newVer){
         	state.status = "<b>New Version Available (Version: $newVerRaw)</b>"
         	log.warn "** There is a newer version of this app available  (Version: $newVerRaw) **"
         	log.warn "** $state.UpdateInfo **"
+             state.newBtn = state.status
        		} 
 		else{ 
       		state.status = "Current"
-      		log.info "You are using the current version of this app"
+       		log.info "You are using the current version of this app"
        		}
       					}
         	} 
         catch (e) {
         	log.error "Something went wrong: CHECK THE JSON FILE AND IT'S URI -  $e"
     		}
- 	
+    if(state.status != "Current"){
+		state.newBtn = state.status
+    }
+    else{
+        state.newBtn = "No Update Available"
+    }
+        
+        
 }
 
+
 def setVersion(){
-		state.version = "1.5.1"	 
+		state.version = "1.5.2"	 
 		state.InternalName = "AverageAllchild"
 }
 
