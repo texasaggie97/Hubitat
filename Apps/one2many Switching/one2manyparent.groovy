7d05acbc15be16fcf27a7d638b36e53839ced5f3
CobraVmax <andrew.parker@securendpoint.com>
2018-10-08 11:46:10 +0100

    08/10/2018

--- Apps/one2many Switching/one2manyparent.groovy
+++ Apps/one2many Switching/one2manyparent.groovy
@@ -33,13 +33,13 @@
  *
  *-------------------------------------------------------------------------------------------------------------------
  *
- *  Last Update: 02/07/2018
+ *  Last Update: 08/10/2018
  *
  *  Changes:
  *
  * 
  *
- *
+ *  V1.0.1 - added revised update checking
  *  V1.0.0 - POC
  *
  */
@@ -82,9 +82,8 @@ def updated() {
 }
 
 def initialize() {
-	setAppVersion()
-    schedule("0 0 14 ? * FRI *", cobra)
-    cobra()
+
+    version()
     log.info "There are ${childApps.size()} child smartapps"
     childApps.each {child ->
     log.info "Child app: ${child.label}"
@@ -95,84 +94,160 @@ def initialize() {
  
 def mainPage() {
     dynamicPage(name: "mainPage") {
-      
-		section {    
-			paragraph image: "https://raw.githubusercontent.com/cobravmax/SmartThings/master/icons/cobra.png",
-				title: "One to Many",
-				required: false,
-				"This parent app is a container for all One To Many child apps"
-			}
-            
-		section{
-            paragraph "Parent Status: $state.verCheck"
-			paragraph "Parent Version: $state.appversion -  $state.copyrightNow"
-			}
-    
-		
-  
-     
-     
-// New Child Apps 
-      
-      
-      
+         installCheck()
         
+if(state.appInstalled == 'COMPLETE'){
+			display()    
+           
+       
       section ("Add An Event"){
 		app(name: "newApp", appName: "One To Many Child", namespace: "Cobra", title: "Add a new event automation child", multiple: true)
-		
-            
-            }
-                  
-   
-           
-           
-// End: New Child Apps
+      }
+    section (" "){}
+  section("App name") {
+        label title: "Enter a name for parent app (optional)", required: false
+            }    
   
-  
-              section("App name") {
-                label title: "Enter a name for parent app (optional)", required: false
-            }
-  
-  
-  
-  
+}
   
  } // DynamicPage 
   
   } // Mainpage
 
-
-
-
-def cobra(){
-    setAppVersion()
-    def paramsUD = [uri: "http://update.hubitat.uk/cobra.json"]
-       try {
-        httpGet(paramsUD) { respUD ->
-  //   log.info "response data: ${respUD.data}"
-       def copyNow = (respUD.data.versions.copyright)
-       state.copyrightNow = copyNow
-       def cobraVer = (respUD.data.versions.(state.InternalName))
-       def cobraOld = state.appversion.replace(".", "")
-       if(cobraOld < cobraVer){
-		state.verCheck = "** New Version Available **"
-           log.warn "There is a newer version of this app available"
-       }    
-       else{ 
-      state.verCheck = "Current"
-       }
-       
-       }
-        } 
-        catch (e) {
-        log.error "Something went wrong: $e"
+def installCheck(){         
+   state.appInstalled = app.getInstallationState() 
+  if(state.appInstalled != 'COMPLETE'){
+section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
+  }
+    else{
+ //       log.info "Parent Installed OK"
     }
-}        
+	}
+
+
+def version(){
+    resetBtnName()
+	schedule("0 0 9 ? * FRI *", updateCheck) //  Check for updates at 9am every Friday
+	updateCheck()  
+    checkButtons()
+}
+
+def display(){
+  
+	if(state.status){
+	section{paragraph "<img src='http://update.hubitat.uk/icons/cobra3.png''</img> Version: $state.version <br><font face='Lucida Handwriting'>$state.Copyright </font>"}
+       
+        }
+    if(state.status != "<b>** This app is no longer supported by $state.author  **</b>"){
+     section(){ input "updateBtn", "button", title: "$state.btnName"}
+    }
+    
+    if(state.status != "Current"){
+	section{ 
+	paragraph "<b>Update Information:</b> <BR>$state.UpdateInfo <BR>$state.updateURI"
+     }
+    }         
+}
+
+def checkButtons(){
+    log.info "Running checkButtons"
+    appButtonHandler("updateBtn")
+}
+
+
+def appButtonHandler(btn){
+    state.btnCall = btn
+    if(state.btnCall == "updateBtn"){
+        log.info "Checking for updates now..."
+        updateCheck()
+        pause(3000)
+  		state.btnName = state.newBtn
+        runIn(2, resetBtnName)
+    }
+    if(state.btnCall == "updateBtn1"){
+    state.btnName1 = "Click Here" 
+    httpGet("https://github.com/CobraVmax/Hubitat/tree/master/Apps' target='_blank")
+    }
+    
+}   
+def resetBtnName(){
+    log.info "Resetting Button"
+    if(state.status != "Current"){
+	state.btnName = state.newBtn
+    }
+    else{
+ state.btnName = "Check For Update" 
+    }
+}    
+    
+def pushOver(inMsg){
+    if(updateNotification == true){  
+     newMessage = inMsg
+  LOGDEBUG(" Message = $newMessage ")  
+     state.msg1 = '[L]' + newMessage
+	speaker.speak(state.msg1)
+    }
+}
+
+
+
+
+
+def updateCheck(){
+    setVersion()
+	def paramsUD = [uri: "http://update.hubitat.uk/cobra.json"]
+       	try {
+        httpGet(paramsUD) { respUD ->
+//   log.warn " Version Checking - Response Data: ${respUD.data}"   // Troubleshooting Debug Code 
+       		def copyrightRead = (respUD.data.copyright)
+       		state.Copyright = copyrightRead
+            def updateUri = (respUD.data.versions.UpdateInfo.GithubFiles.(state.InternalName))
+            state.updateURI = updateUri   
+            
+            def newVerRaw = (respUD.data.versions.Application.(state.InternalName))
+            def newVer = (respUD.data.versions.Application.(state.InternalName).replace(".", ""))
+       		def currentVer = state.version.replace(".", "")
+      		state.UpdateInfo = (respUD.data.versions.UpdateInfo.Application.(state.InternalName))
+                state.author = (respUD.data.author)
+           
+		if(newVer == "NLS"){
+            state.status = "<b>** This app is no longer supported by $state.author  **</b>"  
+             log.warn "** This app is no longer supported by $state.author **" 
+            
+      		}           
+		else if(currentVer < newVer){
+        	state.status = "<b>New Version Available (Version: $newVerRaw)</b>"
+        	log.warn "** There is a newer version of this app available  (Version: $newVerRaw) **"
+        	log.warn "** $state.UpdateInfo **"
+             state.newBtn = state.status
+            def updateMsg = "There is a new version of '$state.ExternalName' available (Version: $newVerRaw)"
+
+       		} 
+		else{ 
+      		state.status = "Current"
+       		log.info "You are using the current version of this app"
+       		}
+      					}
+        	} 
+        catch (e) {
+        	log.error "Something went wrong: CHECK THE JSON FILE AND IT'S URI -  $e"
+    		}
+    if(state.status != "Current"){
+		state.newBtn = state.status
+        
+    }
+    else{
+        state.newBtn = "No Update Available"
+    }
+        
+        
+}
 
 
  
 // App Version   *********************************************************************************
-def setAppVersion(){
-    state.appversion = "1.0.0"
-     state.InternalName = "OneToManyparent"
+def setVersion(){
+    state.version = "1.0.1"
+    state.InternalName = "OneToManyparent"
+	state.ExternalName = "One to Many Parent"
 }
